4267e10cd89876484a3b42c255db9e91e42dfb40
GraceJ0105 <84053064+GraceJ0105@users.noreply.github.com>
2022-03-16 20:16:46 -0700

    Revamp
    Update to condition, wind etc.
    
    Updates to JS

--- src/ap.js
+++ src/ap.js
@@ -35,39 +35,52 @@ function changeTime() {
   changeCurrentDate.innerHTML = `${currentDate}`;
 }
 
-function userCityAlert(event) {
-  event.preventDefault();
-  let city = document.querySelector("#user-city-input");
-  let cityName = document.querySelector("h2.city");
-  cityName.innerHTML = `${city.value}`;
+function search(city) {
   let apiKey = "02a3e153b68431fbb188627f7c3c24b7";
   let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=${units}&appid=${apiKey}`;
-  axios.get(apiUrl).then(showTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
 }
 
-function showTemperature(response) {
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#user-city-input");
+  search(city.value);
+}
+
+function updateWeather(response) {
+  let cityName = document.querySelector("#current-city-name");
   let roundedTemp = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#today-temp");
+  let conditionElement = document.querySelector("#weather-condition");
+  let condition = response.data.weather[0].main;
+  let windElement = document.querySelector("#wind-speed");
+  let wind = Math.round(response.data.wind.speed);
+  let feelsLikeElement = document.querySelector("#feels-like-temp");
+  let feelsLikeTemp = Math.round(response.data.main.feels_like);
+  feelsLikeElement.innerHTML = `${feelsLikeTemp}`;
+  windElement.innerHTML = `${wind}`;
+  conditionElement.innerHTML = `${condition}`;
   temperatureElement.innerHTML = `${roundedTemp}`;
-}
-
-function currentWeather(response) {
-  let roundedTemp = Math.round(response.data.main.temp);
-  let city = response.data.name;
-  let temperatureElement = document.querySelector("#today-temp");
-  let currentCity = document.querySelector("#current-city-name");
-  temperatureElement.innerHTML = roundedTemp;
-  currentCity.innerHTML = city;
+  cityName.innerHTML = `${response.data.name}`;
+  //let sunrise = response.data.sys.sunrise;
+  // Create a new JavaScript Date object based on the timestamp
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+  //let date = new Date(sunrise * 1000);
+  // Hours part from the timestamp
+  //let hours = date.getHours();
+  // Minutes part from the timestamp
+  //let minutes = "0" + date.getMinutes();
+  // Will display time in 10:30 format
+  //let formattedSunriseTime = hours + ":" + minutes.substr(-2);
+  // expected output "3:19:27 PM"
+  //console.log(formattedSunriseTime);
 }
 
 function showPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
   let apiKey = "02a3e153b68431fbb188627f7c3c24b7";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
-  axios.get(apiUrl).then(currentWeather);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
 }
 
 function getCurrentLocation(event) {
@@ -77,8 +90,8 @@ function getCurrentLocation(event) {
 
 changeTime();
 
-let form = document.querySelector("#user-city-form");
-form.addEventListener("submit", userCityAlert);
-
 let currentLocation = document.querySelector("#current-weather");
 currentLocation.addEventListener = ("click", getCurrentLocation);
+
+let form = document.querySelector("#user-city-form");
+form.addEventListener("submit", handleSubmit);
